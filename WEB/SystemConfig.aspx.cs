01f8b95dda90e08a492c0634d87767bef03a7638
王凡 <hbh2010@126.com>
2015-04-24 15:06:10 +0800

    mysql.*.dll reference
    由全局改为指定引用
    2015-04-24 by wf

--- WEB/SystemConfig.aspx.cs
+++ WEB/SystemConfig.aspx.cs
@@ -11,4 +11,28 @@ public partial class SystemConfig : System.Web.UI.Page
     {
 
     }
+    protected void GridView_RowDataBound(object sender, GridViewRowEventArgs e)
+    {
+
+    }
+    protected void GridView_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+    {
+
+    }
+    protected void GridView_RowDeleting(object sender, GridViewDeleteEventArgs e)
+    {
+
+    }
+    protected void GridView_RowEditing(object sender, GridViewEditEventArgs e)
+    {
+
+    }
+    protected void GridView_RowUpdating(object sender, GridViewUpdateEventArgs e)
+    {
+
+    }
+    protected void GridView_PageIndexChanging(object sender, GridViewPageEventArgs e)
+    {
+
+    }
 }
